b2eb54798bf7148fd83e24bca5c3abcdc502ee17
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-09-12 21:33:59 -0300

    aplicando desconto pix

--- js/script.js
+++ js/script.js
@@ -5,19 +5,29 @@ const pix = document.getElementById("pix");
 const credito = document.getElementById("credito");
 const divPix = document.getElementsByClassName("container-pix")[0];
 const divCredito = document.getElementsByClassName("container-credito")[0];
+const valorPix = document.getElementById("valor-pix");
 
-informarDados.addEventListener("click", function(){
-    if(valor.value == ""){
-        alert("campo deve ser preenchido!");
+informarDados.addEventListener("click", function() {
+    if (valor.value == "") {
+        alert("Campo deve ser preenchido!");
         return;
-    } else if(pix.checked && valor.value !== ""){
+    }
+
+    // Calcula o valor com desconto
+    let desconto = 10 / 100;
+    let valorNumerico = parseFloat(valor.value);
+    let valorComDesconto = valorNumerico - (valorNumerico * desconto);
+
+    if (pix.checked) {
         divPix.style.display = "block";
         divPix.style.marginTop = "10px";
-    }else if(credito.checked && valor.value !== ""){
+        valorPix.textContent = "Total: " + valorComDesconto.toFixed(2); // Limita o número de casas decimais
+    } else if (credito.checked) {
         divCredito.style.display = "block";
         divCredito.style.marginTop = "10px";
     }
 });
+
 
 // validadando cartao de credito
 const numeroCartaoInput = document.getElementById("numero-cartao");
